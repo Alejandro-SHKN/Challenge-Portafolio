1d45f2c3161df9a9725d6779ab16acb1ecfc2c79
Alejandro <alejandroshkn@gmail.com>
2023-05-02 19:37:08 -0500

    Se agrego animación con JS a la seccion Skills 2/6

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,12 +1,40 @@
 // Javascript aplicado a la seccion SKILLS
 // HTML5
-let skillsContainer = document.querySelector('.div__exteriorskills');
+let divHTMLoriginal = document.getElementById(`ID-div__containerLogoyTextoHTML--Skills`)
+let divHTMLModificado = document.getElementById(`ID-preload-div__containerLogoyTextoHTML--Skills`)
 
-skillsContainer.addEventListener('click', function(event) {
-    let html5Div = document.getElementById('html5div');
-    html5Div.style.display = `flex`
-    html5Div.style.flexWrap = `wrap`
-    html5Div.style.justifyContent = `center`
+  divHTMLoriginal.addEventListener(`click`, function(){
+    if (divHTMLoriginal.style.display !== `none`){
+      divHTMLoriginal.style.display = `none`
+      divHTMLModificado.style.display = `flex`
+    } 
+  });
 
-    html5Div.innerHTML = "<p class=JS-P__infoHTML5--Skills>Comencé con esto y a partir de aquí descubrí mi amor por las páginas web </p> <button id=botonCerrar>cerrar</button>";
-});
+  divHTMLModificado.addEventListener(`click`, function(){
+    if(divHTMLModificado.style.display != `none`){
+      divHTMLModificado.style.display = `none`
+      divHTMLoriginal.style.display = `block`
+    }
+  })
+
+// Javascript
+let divJSoriginal = document.getElementById(`IDdiv__containerLogoyTextoJS--Skills`)
+let divJSModificado = document.getElementById(`ID-preload-div__containerLogoyTextoJS--Skills`)
+ divJSoriginal.addEventListener(`click`, function(){
+  if(divJSoriginal.style.opacity != `1`){
+    divJSoriginal.style.opacity = 0
+    divJSModificado.style.opacity = 1;
+    divJSModificado.style.zIndex = 1;
+    divJSModificado.style.marginBottom = `60px`;
+  }
+})
+
+divJSModificado.addEventListener(`click`, function(){
+  if(divJSModificado.style.opacity == 1){
+    divJSoriginal.style.opacity = 2
+    divJSModificado.style.opacity = 0
+    divJSModificado.style.zIndex = -1;
+    divJSModificado.style.marginBottom = `0`;
+    
+    }
+ })
